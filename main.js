018f4603b497c76ab6dd7f15d6cb293686ab0918
Elementals888 <ecxscariot@gmail.com>
2022-11-23 15:11:57 -0600

    First commit

---
+++ main.js
@@ -0,0 +1,44 @@
+/* developed by EcXscariot/yourOrdinaryCarrot */
+/* last modified 11/23/2022 */
+
+const navMenu = document.getElementById('nav-menu');
+const navButtons = document.querySelector('.nav-buttons');
+
+
+function main() {
+    navBtn();
+    // resetAnimation();    
+}
+
+// function resetAnimation() {
+//     navMenu.addEventListener('animationend', () => { 
+//         let timer = setTimeout(animateBack, 60000);
+//         if (navMenu.getAttribute('data-pressed') == 'off') {
+//             clearTimeout(timer);
+//         }
+//     });
+// }
+
+
+function navBtn() {
+    navMenu.addEventListener("click", () => {
+        const isClicked = navMenu.getAttribute('data-pressed');
+        if (!isClicked || isClicked == 'off') {
+            navMenu.setAttribute('data-pressed', 'on');
+            navButtons.setAttribute('data-pressed', 'on');
+        } else {
+            animateBack();
+        }
+        navMenu.disabled = true;
+        let buttonTimer = setTimeout(()=> {
+            navMenu.disabled = false;
+        }, 700)
+    });
+}
+
+function animateBack(){
+    navMenu.setAttribute('data-pressed', 'off');
+    navButtons.setAttribute('data-pressed', 'off');
+}
+
+main();
